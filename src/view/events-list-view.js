c2fb64ee993fd2bf07c58e47da1259c86b026da0
Alice <devo4ka2410@gmail.com>
2022-05-06 16:46:31 +0300

    refactor: add horizontal indentation

--- src/view/events-list-view.js
+++ src/view/events-list-view.js
@@ -1,6 +1,8 @@
 import { createElement } from '../render';
 
-const createEventsListTemplate = () => ('<ul class="trip-events__list"></ul>');
+const createEventsListTemplate = () => (
+  '<ul class="trip-events__list"></ul>'
+);
 
 export default class EventsListView {
   getTemplate() {
